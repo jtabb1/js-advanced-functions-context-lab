a340cc22bb14f4e87094d17b61e2c7d820b5611c
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-04-11 02:26:31 -0400

    Pass 16 of 35 tests so far

--- index.js
+++ index.js
@@ -9,6 +9,47 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecord(arr) {
+    return {
+        firstName : arr[0],
+        familyName : arr[1],
+        title : arr[2],
+        payPerHour : arr[3],
+        timeInEvents : [],
+        timeOutEvents : []
+    }
+};
+
+function createEmployeeRecords(arr) {
+    const retArr = [];
+    for (let i=0; i < arr.length; i++) {
+        retArr.push(createEmployeeRecord(arr[i]));
+    }
+    return retArr;
+};
+
+function createTimeInEvent(employeeRecord,dateTimeString) {
+    const arr = dateTimeString.split(' ');
+    const obj = {
+        type : 'TimeIn',
+        hour : parseInt(arr[1]),
+        date : arr[0]
+    };
+    employeeRecord.timeInEvents.push(obj);
+    return employeeRecord;
+}
+
+function createTimeOutEvent(employeeRecord,dateTimeString) {
+    const arr = dateTimeString.split(' ');
+    const obj = {
+        type : 'TimeOut',
+        hour : parseInt(arr[1]),
+        date : arr[0]
+    };
+    employeeRecord.timeOutEvents.push(obj);
+    return employeeRecord;
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
