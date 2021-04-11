b0d8bb7913d02d9046c6e1e16f5e7990dd69536b
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-04-11 10:18:59 -0400

    Draft complete solution with debugging efforts

--- index.js
+++ index.js
@@ -28,26 +28,44 @@ function createEmployeeRecords(arr) {
     return retArr;
 };
 
-function createTimeInEvent(employeeRecord,dateTimeString) {
+function createTimeInEvent(dateTimeString) {
     const arr = dateTimeString.split(' ');
     const obj = {
         type : 'TimeIn',
         hour : parseInt(arr[1]),
         date : arr[0]
     };
-    employeeRecord.timeInEvents.push(obj);
-    return employeeRecord;
+    this.timeInEvents.push(obj);
+    return this;
 }
 
-function createTimeOutEvent(employeeRecord,dateTimeString) {
+function createTimeOutEvent(dateTimeString) {
     const arr = dateTimeString.split(' ');
     const obj = {
         type : 'TimeOut',
         hour : parseInt(arr[1]),
         date : arr[0]
     };
-    employeeRecord.timeOutEvents.push(obj);
-    return employeeRecord;
+    this.timeOutEvents.push(obj);
+    return this;
+}
+
+function hoursWorkedOnDate(dateStr) {
+    const allPunchIns = this.timeInEvents;
+    const allPunchOuts = this.timeOutEvents;
+    // In the real world there could be more than one punch in and out in a day,
+    //   but not in this example.
+    const datePunchIn = allPunchIns.filter(punch => punch.date === dateStr);
+    const punchIn = datePunchIn[0].hour;
+    const datePunchOut = allPunchOuts.filter(punch => punch.date === dateStr);
+    const punchOut = datePunchOut[0].hour;
+    return (punchOut - punchIn)/100;
+}
+
+function wagesEarnedOnDate(dateStr) {
+    const hours = hoursWorkedOnDate.call(this,dateStr);
+    const rate = this.payPerHour;
+    return rate * hours;
 }
 
 let allWagesFor = function () {
@@ -61,3 +79,97 @@ let allWagesFor = function () {
 
     return payable
 }
+
+// function calculatePayroll(ees) {
+//     return ees.reduce( (acc, cur) => {
+//         console.log(acc);
+//         return acc + allWagesFor(cur);
+//     }, 0);
+// }
+
+function calculatePayroll(ees) {
+    return ees.reduce( (acc, cur) => acc + allWagesFor.call(cur), 0);
+}
+
+function findEmployeeByFirstName(ees, fn) {
+    return ees.filter( ee => ee.firstName === fn )[0];  // Returns first result of the array returned by the filter
+}
+
+
+
+
+let rRecord = createEmployeeRecord(["Rafiki", "", "Aide", 10])
+let sRecord = createEmployeeRecord(["Simba", "", "King", 100])
+
+let sTimeData = [
+  ["2019-01-01 0900", "2019-01-01 1300"], // 4 * 100 = 400
+  ["2019-01-02 1000", "2019-01-02 1300"]  // 3 * 100 = 300 ===> 700 total
+]
+
+let rTimeData = [
+  ["2019-01-11 0900", "2019-01-11 1300"], // 4 * 10 = 40
+  ["2019-01-12 1000", "2019-01-12 1300"]  // 3 * 10 = 40 ===> 70 total ||=> 770
+]
+
+sTimeData.forEach(function (d) {
+  let [dIn, dOut] = d
+  createTimeInEvent.call(sRecord, dIn)
+  createTimeOutEvent.call(sRecord, dOut)
+})
+
+rTimeData.forEach(function (d, i) {
+  let [dIn, dOut] = d
+  createTimeInEvent.call(rRecord, dIn)
+  createTimeOutEvent.call(rRecord, dOut)
+})
+
+let grandTotalOwed = [sRecord, rRecord].reduce((m, e) => m + allWagesFor.call(e), 0)
+
+const csvDataEmployees = [
+    ["Thor", "Odinsson", "Electrical Engineer", 45],
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+    ["Natalia", "Romanov", "CEO", 150],
+    ["Darcey", "Lewis", "Intern", 15],
+    ["Jarvis", "Stark", "CIO", 125],
+    ["Anthony", "Stark", "Angel Investor", 300]
+]
+
+const csvTimesIn = [
+    ["Thor", ["2018-01-01 0800", "2018-01-02 0800", "2018-01-03 0800"]],
+    ["Loki", ["2018-01-01 0700", "2018-01-02 0700", "2018-01-03 0600"]],
+    ["Natalia", ["2018-01-03 1700", "2018-01-05 1800", "2018-01-03 1300"]],
+    ["Darcey", ["2018-01-01 0700", "2018-01-02 0800", "2018-01-03 0800"]],
+    ["Jarvis", ["2018-01-01 0500", "2018-01-02 0500", "2018-01-03 0500"]],
+    ["Anthony", ["2018-01-01 1400", "2018-01-02 1400", "2018-01-03 1400"]]
+]
+
+const csvTimesOut = [
+    ["Thor", ["2018-01-01 1600", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Loki", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Natalia", ["2018-01-03 2300", "2018-01-05 2300", "2018-01-03 2300"]],
+    ["Darcey", ["2018-01-01 1300", "2018-01-02 1300", "2018-01-03 1300"]],
+    ["Jarvis", ["2018-01-01 1800", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Anthony", ["2018-01-01 1600", "2018-01-02 1600", "2018-01-03 1600"]]
+]
+
+let employeeRecords = createEmployeeRecords(csvDataEmployees)
+employeeRecords.forEach(function (rec) {
+let timesInRecordRow = csvTimesIn.find(function (row) {
+    return rec.firstName === row[0]
+})
+
+let timesOutRecordRow = csvTimesOut.find(function (row) {
+    return rec.firstName === row[0]
+})
+
+timesInRecordRow[1].forEach(function(timeInStamp){
+    createTimeInEvent.call(rec, timeInStamp)
+})
+
+timesOutRecordRow[1].forEach(function(timeOutStamp){
+    createTimeOutEvent.call(rec, timeOutStamp)
+})
+}) 
+
+
+// console.log(calculatePayroll(employeeRecords));
